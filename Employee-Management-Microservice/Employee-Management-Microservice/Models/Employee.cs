1e8858babc89efed90420436af16b4313c36c38c
Samkeliso ndlangamandla <samkelisojam285@gmail.com>
2025-05-05 21:39:21 +0200

    Employee Model
    define user model property

---
+++ Employee-Management-Microservice/Employee-Management-Microservice/Models/Employee.cs
@@ -0,0 +1,70 @@
+﻿namespace Employee_Management_Microservice.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
+
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        [ForeignKey("ReportingManager")]
+        public int? ReportingManager { get; set; }
+        public Employee Manager { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string CompanyId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Phone]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime HireDate { get; set; }
+    }
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string RoleName { get; set; }
+    }
+
+}
